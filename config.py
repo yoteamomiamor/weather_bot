d0f943172d98292b2aa0dfc71addbc15261b8a54
kirill <sknxq1@gmail.com>
2024-05-10 23:16:55 +0300

    update config: add langugages path

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from environs import Env
 from dataclasses import dataclass
 from typing import Optional
+from os.path import join
 
 
 @dataclass
@@ -11,9 +12,13 @@ class Bot:
 @dataclass
 class Config:
     bot: Bot
+    languages: str
 
 
 def load_config(path: Optional[str] = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(Bot(env('BOT_TOKEN')))
+    return Config(
+        bot=Bot(env('BOT_TOKEN')),
+        languages=(join('.', 'locales', 'languages'))
+        )
