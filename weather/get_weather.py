2af2a15d2f55e8e4d2eea21958efacfa62a42355
kirill <sknxq1@gmail.com>
2024-05-11 12:22:48 +0300

    update placeholder functions

--- weather/get_weather.py
+++ weather/get_weather.py
@@ -3,3 +3,11 @@ from fluentogram import TranslatorRunner
 
 def today_weather(lang: TranslatorRunner) -> str:
     return 'today\'s weather is good'
+
+
+def tomorrow_weather(lang: TranslatorRunner) -> str:
+    return 'tomorrow\'s weather is good'
+
+
+def week_weather(lang: TranslatorRunner) -> str:
+    return 'weekly weather is good'
