a4094a8cac04920eb9a6fc7978582347d0261b03
kirill <sknxq1@gmail.com>
2024-05-11 12:27:58 +0300

    delete unnecessary

--- keyboards/callback_factory.py
+++ keyboards/callback_factory.py
@@ -1,6 +1,6 @@
 from aiogram.filters.callback_data import CallbackData
 
 
-class WeatherCallback(CallbackData):
+class WeatherCallback(CallbackData, prefix='weather_date'):
     date: int
     days: int
