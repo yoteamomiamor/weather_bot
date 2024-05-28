8eac3be48fec0c5eec60b5f219409959d5df16de
kirill <sknxq1@gmail.com>
2024-05-28 20:22:41 +0300

    update weather handlers

--- handlers/weather_handlers.py
+++ handlers/weather_handlers.py
@@ -12,65 +12,49 @@ from db.models import User
 from weather.get_weather import *
 from handlers.states import MainFSM
 
+from typing import Callable
+
 
 rt = Router()
+
+
+async def request_weather(message: Message, session: AsyncSession, 
+                          i18n: I18nContext, weather_getter: Callable):
+    user_id = message.from_user.id
+    sql_query = (select(User.lat, User.long)
+                 .where(User.user_id == user_id)
+                 .limit(1))
+    data = await session.execute(sql_query)
+    await session.commit()
+    
+    for row in data:
+        return await weather_getter(
+                i18n=i18n, 
+                latitude=row.lat, 
+                longitude=row.long
+            )
 
 # today
 @rt.message(MainFSM.select_weather, F.text == LazyProxy('weather_today'))
 async def process_today_button(message: Message, state: FSMContext,
                                i18n: I18nContext, session: AsyncSession):
-    user_id = message.from_user.id
-    sql_query = (select(User.lat, User.long)
-                 .where(User.user_id == user_id)
-                 .limit(1))
-    data = await session.execute(sql_query)
-    await session.commit()
+    await message.answer(
+        text=await request_weather(message, session, i18n, get_today_weather)
+    )
     
-    for row in data:
-        await message.answer(
-            text=await get_today_weather(
-                i18n=i18n, 
-                latitude=row.lat, 
-                longitude=row.long
-            )
-        )
-
+    
 # tomorrow
 @rt.message(MainFSM.select_weather, F.text == LazyProxy('weather_tomorrow'))
 async def process_tomorrow_button(message: Message, state: FSMContext,
                                i18n: I18nContext, session: AsyncSession):
-    user_id = message.from_user.id
-    sql_query = (select(User.lat, User.long)
-                 .where(User.user_id == user_id)
-                 .limit(1))
-    data = await session.execute(sql_query)
-    await session.commit()
-    
-    for row in data:
-        await message.answer(
-            text=await get_tomorrow_weather(
-                i18n=i18n, 
-                latitude=row.lat, 
-                longitude=row.long
-            )
-        )
+    await message.answer(
+        text=await request_weather(message, session, i18n, get_tomorrow_weather)
+    )
 
 # week
 @rt.message(MainFSM.select_weather, F.text == LazyProxy('weather_week'))
 async def process_week_button(message: Message, state: FSMContext,
                                i18n: I18nContext, session: AsyncSession):
-    user_id = message.from_user.id
-    sql_query = (select(User.lat, User.long)
-                 .where(User.user_id == user_id)
-                 .limit(1))
-    data = await session.execute(sql_query)
-    await session.commit()
-    
-    for row in data:
-        await message.answer(
-            text=await get_week_weather(
-                i18n=i18n, 
-                latitude=row.lat, 
-                longitude=row.long
-            )
-        )
+    await message.answer(
+        text=await request_weather(message, session, i18n, get_week_weather)
+    )
